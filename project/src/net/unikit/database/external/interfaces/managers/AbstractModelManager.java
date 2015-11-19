8485888b962ceea99acd548070a4262d357d35a9
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-19 19:55:42 +0100

    Small fix.

--- project/src/net/unikit/database/external/interfaces/managers/AbstractModelManager.java
+++ project/src/net/unikit/database/external/interfaces/managers/AbstractModelManager.java
@@ -1,11 +1,13 @@
 package net.unikit.database.external.interfaces.managers;
 
+import net.unikit.database.external.interfaces.entities.AbstractModel;
+
 import java.util.List;
 
 /**
  * Created by Andreas on 19.11.2015.
  */
-public interface AbstractModelManager<EntityType, IdType> {
+public interface AbstractModelManager<EntityType extends AbstractModel, IdType> {
     List<EntityType> getAllEntities();
     EntityType getEntity(IdType id);
     void updateEntity(EntityType entity);
